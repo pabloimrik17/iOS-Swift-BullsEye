c91dcae39a2e079236eadf16079002325f7064b6
Pablo Fernandez Guerra <pabloimrik17@gmail.com>
2019-03-30 20:19:25 +0100

    IBOutlet for the slider. currentValue no has the default value of the slider when the view starts

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var slider: UISlider!
     var currentValue: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        currentValue = Int(slider.value.rounded())
         // Do any additional setup after loading the view, typically from a nib.
     }
     
