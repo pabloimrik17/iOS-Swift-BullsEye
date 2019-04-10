8c85fdf61c0fd64a0cdc8d6a1ad3f5bf23a8e4aa
Pablo Fernandez Guerra <pabloimrik17@gmail.com>
2019-04-11 00:15:06 +0200

    Added webview to show the about info text with html/css instead plain css.

--- BullsEye/AboutViewController.swift
+++ BullsEye/AboutViewController.swift
@@ -7,11 +7,23 @@
 //
 
 import UIKit
+import WebKit
 
 class AboutViewController: UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadAboutWebView()
+    }
+    
+    func loadAboutWebView () {
+        if let htmlPath = Bundle.main.path(forResource: "BullsEye", ofType: "html") {
+            let url = URL(fileURLWithPath: htmlPath)
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
     }
     
     @IBAction func onClickCloseButton() {
